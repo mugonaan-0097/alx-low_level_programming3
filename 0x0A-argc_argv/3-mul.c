30446419bc72d4c6a8ec54b0f41c05a4a20d4d9c
mugonaan-0097 <mugonaan@gmail.com>
2022-10-05 07:44:49 -0700

    waa

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  program that multiplies two numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true and 1 if false
+ */
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+
+	if (argc == 3)
+	{
+		mul *= atoi(argv[argc - 2]);
+		mul *= atoi(argv[argc - 1]);
+		printf("%d\n", mul);
+	}
+	else
+	prinf("Error\n");
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: The number of command line arguments
+ * @argv: An array containing the progress command line argument
+ *
+ * Return: Nothing.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, j;
+
+	if (argc > 0)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!(isdigit(argv[i][j])))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+	return (0);
+}
