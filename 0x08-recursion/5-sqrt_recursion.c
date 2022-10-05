97d0e9e6dbc59b0961f23d28c4f947945ec911f6
mugonaan-0097 <mugonaan@gmail.com>
2022-10-05 16:10:18 -0700

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * sqrt_a - function that returns the natural square root of a number.
+ * @a: number
+ * @b: number of power
+ * Return: Always.
+ */
+int sqrt_a(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	return (sqrt_a(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root.
+ * @n: number
+ *
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_a(n, 0));
+}
