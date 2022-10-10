b804eed660889d5c56431e877f7d491a6f186e52
mugonaan-0097 <mugonaan@gmail.com>
2022-10-10 12:03:50 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file it was
+ * compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("File :%s\n", _FILE_);
+	return (0);
+}
