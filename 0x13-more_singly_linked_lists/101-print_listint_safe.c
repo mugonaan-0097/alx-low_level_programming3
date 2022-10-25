1c37da5fdc2c93021731229aa5d8c6ba93786760
mugonaan-0097 <mugonaan@gmail.com>
2022-10-25 10:31:44 -0700

    print safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -47,3 +47,35 @@ size_t looped_listint_len(const listint_t *head)
 	}
 	return (0);
 }
+
+/**
+ * print_listint_safe -  function that prints a listint_t linked list.
+ * @head: A pointer to the listint_t list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes, index = 0;
+
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (nodes);
+}
