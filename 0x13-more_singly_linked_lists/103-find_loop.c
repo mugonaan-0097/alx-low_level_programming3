695d6a30d9b67a9e63919c8f2c211ec43b2da432
mugonaan-0097 <mugonaan@gmail.com>
2022-10-25 13:47:00 -0700

    sneak

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -9,29 +9,28 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *tortoise, *hare;
+	listint_t *node1, *node2;
 
 	if (head == NULL || head->next == NULL)
 		return (NULL);
 
-	tortoise = head->next;
-	hare = (head->next)->next;
+	node1 = head->next;
+	node2 = (head->next)->next;
 
-	while (hare)
+	while (node2)
 	{
-		if (tortoise == hare)
+		if (node1 == node2)
 		{
-			tortoise = head;
-
-			while (tortoise != hare)
+			node1 = head;
+			while (node1 != node2)
 			{
-				tortoise = tortoise->next;
-				hare = hare->next;
+				node1 = node1->next;
+				node2 = node2->next;
 			}
-			return (tortoise);
+			return (node1);
 		}
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
+		node1 = node1->next;
+		node2 = (node2->next)->next;
 	}
 	return (NULL);
 }
