80559c9a2c8f5f9b11356646cfe74dd3e13dcdf2
mugonaan-0097 <mugonaan@gmail.com>
2022-10-31 17:53:19 -0700

    boom

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -57,6 +57,7 @@ int main(int argc, char *argv[])
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
+
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
@@ -64,6 +65,7 @@ int main(int argc, char *argv[])
 			"Error: can't close fd %d\n", file_from);
 		exit(100);
 	}
+
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
